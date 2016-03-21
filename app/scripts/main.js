9682c59a636f452bf0d3939cc5317e3a252685cd
Daniel White <daniel@developerdan.com>
2016-03-20 20:09:39 -0400

    Looks like the original site is back.

---
+++ app/scripts/main.js
@@ -0,0 +1,12 @@
+function() {
+  $('.gallery').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+      type: 'image',
+      delegate: 'a', // the selector for gallery item
+      gallery: {
+        enabled:true
+      }
+    });
+  });
+}
+
